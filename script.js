77e5eaa69a0da67eae097319502d072ee78ff914
Kyarix <ashley.nanami@gmail.com>
2026-07-22 14:28:19 -0300

    cambios precios

--- script.js
+++ script.js
@@ -4,48 +4,48 @@ const WSP_NUMBER = "5493413514606";
 // --- BASE DE DATOS COMPLETA Y CORREGIDA ---
 const productos = [
     // ENTRADAS
-    { id: 1, cat: "Entradas", nombre: "Onigiri Veggie (Pack x2)", precio: 8000, ingredientes: "Relleno de zanahoria, pepino y queso. Envuelto en sésamo.", imagen: "imagenes/OnigiriVeggie.jpeg", piezas: "2 Unid." },
-    { id: 2, cat: "Entradas", nombre: "Tuna Mayo (Pack x2)", precio: 8000, ingredientes: "Relleno de lomo de Atún con mayonesa y ciboutelle. Envuelto en sésamo.", imagen: "imagenes/Onigiri.jfif", piezas: "2 Unid." },
-    { id: 10, cat: "Entradas", nombre: "Tori Katsu (Pack x2)", precio: 8000, ingredientes: "Relleno de bastoncitos de pollo rebozados en panko, con mayo spice. Envuelto en sésamo.", imagen: "imagenes/Onigiri.jfif", piezas: "2 Unid." },
-    { id: 11, cat: "Entradas", nombre: "Ebi Crunch (Pack x2)", precio: 9000, ingredientes: "Relleno de langostinos crocantes rebozados en panko, con mayo spice. Envuelto en sésamo.", imagen: "imagenes/Onigiri.jfif", piezas: "2 Unid.", popular: true },
+    { id: 1, cat: "Entradas", nombre: "Onigiri Veggie (Pack x2)", precio: 9000, ingredientes: "Relleno de zanahoria, pepino y queso. Envuelto en sésamo.", imagen: "imagenes/OnigiriVeggie.jpeg", piezas: "2 Unid." },
+    { id: 2, cat: "Entradas", nombre: "Tuna Mayo (Pack x2)", precio: 9000, ingredientes: "Relleno de lomo de Atún con mayonesa y ciboutelle. Envuelto en sésamo.", imagen: "imagenes/Onigiri.jfif", piezas: "2 Unid." },
+    { id: 10, cat: "Entradas", nombre: "Tori Katsu (Pack x2)", precio: 9000, ingredientes: "Relleno de bastoncitos de pollo rebozados en panko, con mayo spice. Envuelto en sésamo.", imagen: "imagenes/Onigiri.jfif", piezas: "2 Unid." },
+    { id: 11, cat: "Entradas", nombre: "Ebi Crunch (Pack x2)", precio: 10000, ingredientes: "Relleno de langostinos crocantes rebozados en panko, con mayo spice. Envuelto en sésamo.", imagen: "imagenes/Onigiri.jfif", piezas: "2 Unid.", popular: true },
     
     // ONI BURGERS
-    { id: 21, cat: "Entradas", nombre: "Oni Burger Pollo (Pack x2)", precio: 10000, ingredientes: "Rebozado en panko y frito. Relleno de bastoncitos de pollo rebozados en panko, con mayo spice.", imagen: "imagenes/OniBurger.jfif", piezas: "2 Unid.", popular: true },
-    { id: 22, cat: "Entradas", nombre: "Oni Burger Atún (Pack x2)", precio: 10000, ingredientes: "Rebozado en panko y frito. Relleno de lomo de Atún con mayonesa.", imagen: "imagenes/OniBurger.jfif", piezas: "2 Unid.", popular: true },
-    { id: 23, cat: "Entradas", nombre: "Oni Burger Kanikama (Pack x2)", precio: 10000, ingredientes: "Rebozado en panko y frito. Relleno de Kanikama y mayonesa.", imagen: "imagenes/OniBurger.jfif", piezas: "2 Unid." },
+    { id: 21, cat: "Entradas", nombre: "Oni Burger Pollo (Pack x2)", precio: 11000, ingredientes: "Rebozado en panko y frito. Relleno de bastoncitos de pollo rebozados en panko, con mayo spice.", imagen: "imagenes/OniBurger.jfif", piezas: "2 Unid.", popular: true },
+    { id: 22, cat: "Entradas", nombre: "Oni Burger Atún (Pack x2)", precio: 11000, ingredientes: "Rebozado en panko y frito. Relleno de lomo de Atún con mayonesa.", imagen: "imagenes/OniBurger.jfif", piezas: "2 Unid.", popular: true },
+    { id: 23, cat: "Entradas", nombre: "Oni Burger Kanikama (Pack x2)", precio: 11000, ingredientes: "Rebozado en panko y frito. Relleno de Kanikama y mayonesa.", imagen: "imagenes/OniBurger.jfif", piezas: "2 Unid." },
 
     // COMBOS
-    { id: 3, cat: "Combos", nombre: "Barquito Premium 40 (SIN STOCK)", precio: 58000, ingredientes: "Surtido de Rolls fríos. Contiene 8 Red Pasion, 8 Ebi Furai, 8 Karaage Roll, 8 Tuna Deluxe y 8 Sweet Kani. Incluye 4 juegos de palillos y 4 salsas de soja y 2 de maracuyá. (SIN STOCK)", imagen: "imagenes/Combo1.jpg", piezas: "40 Piezas", popular: true },
-    { id: 18, cat: "Combos", nombre: "Barquito Clásico 16", precio: 28000, ingredientes: "Surtido de Rolls fríos. Contiene 8 Red Pasion y 8 Ebi Furai. Incluye 2 juegos de palillos, 2 salsas de soja y 1 salsa de maracuyá.", imagen: "imagenes/Combo16Piezas.jpg", piezas: "16 Piezas" },
-    { id: 19, cat: "Combos", nombre: "Barquito Deluxe 16", precio: 29000, ingredientes: "Full Salmón rosado, palta y queso. Incluye 2 juegos de palillos, 2 salsas de soja y 1 salsa de maracuyá.", imagen: "imagenes/ComboSalmon.jfif", piezas: "16 Piezas", popular: true },
+    { id: 3, cat: "Combos", nombre: "Barquito Premium 40 (SIN STOCK)", precio: 60000, ingredientes: "Surtido de Rolls fríos. Contiene 8 Red Pasion, 8 Ebi Furai, 8 Karaage Roll, 8 Tuna Deluxe y 8 Sweet Kani. Incluye 4 juegos de palillos y 4 salsas de soja y 2 de maracuyá. (SIN STOCK)", imagen: "imagenes/Combo1.jpg", piezas: "40 Piezas", popular: true },
+    { id: 18, cat: "Combos", nombre: "Barquito Clásico 16", precio: 29000, ingredientes: "Surtido de Rolls fríos. Contiene 8 Red Pasion y 8 Ebi Furai. Incluye 2 juegos de palillos, 2 salsas de soja y 1 salsa de maracuyá.", imagen: "imagenes/Combo16Piezas.jpg", piezas: "16 Piezas" },
+    { id: 19, cat: "Combos", nombre: "Barquito Deluxe 16", precio: 30000, ingredientes: "Full Salmón rosado, palta y queso. Incluye 2 juegos de palillos, 2 salsas de soja y 1 salsa de maracuyá.", imagen: "imagenes/ComboSalmon.jfif", piezas: "16 Piezas", popular: true },
 
     // ROLLS ESPECIALES 
-    { id: 30, cat: "Especiales", nombre: "Salmón Avocado Deluxe", precio: 15500, ingredientes: "Roll relleno de salmón fresco, pepino y queso crema. Con cobertura de finas fetas de palta con un toque de mayo spicy.", imagen: "imagenes/Avocado.jpeg", piezas: "8 Piezas", popular: true },
-    { id: 31, cat: "Especiales", nombre: "Kani Fresh", precio: 13500, ingredientes: "Roll relleno de kanikama, pepino y queso crema. Con cobertura de finas fetas de palta y semillas de sésamo.", imagen: "imagenes/.jpeg", piezas: "8 Piezas" },
-    { id: 32, cat: "Especiales", nombre: "Avocado Pink Fusion", precio: 15500, ingredientes: "Roll relleno de kanikama, palta y queso crema. Con cobertura de finas fetas de salmón rosado fresco.", imagen: "imagenes/.jpeg", piezas: "8 Piezas", popular: true },
-    { id: 33, cat: "Especiales", nombre: "Black Pasion", precio: 15000, ingredientes: "Roll relleno de salmón rosado fresco, queso crema con cebollita de verdeo y palta. Con cobertura de sésamo negro.", imagen: "imagenes/BlackPasion.jpeg", piezas: "8 Piezas" },
-    { id: 34, cat: "Especiales", nombre: "Ebi Red Pasion", precio: 15500, ingredientes: "Roll relleno de langostinos rebozados en panko, queso crema y palta. Cubierto con finas fetas de salmón rosado fresco.", imagen: "imagenes/SalmonCobertura.jpeg", piezas: "8 Piezas", popular: true },
-    { id: 35, cat: "Especiales", nombre: "Palmito Fresh", precio: 12000, ingredientes: "Roll relleno de palmitos, queso crema y palta.", imagen: "imagenes/Palmito.jpeg", piezas: "8 Piezas" },
-    { id: 36, cat: "Especiales", nombre: "California Roll", precio: 12000, ingredientes: "Roll relleno de kanikama, palta y bastoncitos de pepino.", imagen: "imagenes/California.jpg", piezas: "8 Piezas", popular: true },
-    { id: 37, cat: "Especiales", nombre: "Sweet Red (SIN STOCK)", precio: 15500, ingredientes: "Roll relleno de salmón rosado fresco, queso crema y frutilla. Con cobertura de sésamo negro. (Según disponibilidad de fruta de estación). (SIN STOCK)", imagen: "imagenes/SweetRed.jpg", piezas: "8 Piezas", popular: true },
-    //{ id: 38, cat: "Especiales", nombre: "Tropical Roll", precio: 14500, ingredientes: "Roll relleno de salmón rosado fresco, queso crema y mango. Con cobertura de sésamo negro. (Según disponibilidad de fruta de estación)", imagen: "imagenes/Philly.jpg", piezas: "8 Piezas", popular: true },
+    { id: 30, cat: "Especiales", nombre: "Salmón Avocado Deluxe", precio: 16000, ingredientes: "Roll relleno de salmón fresco, pepino y queso crema. Con cobertura de finas fetas de palta con un toque de mayo spicy.", imagen: "imagenes/Avocado.jpeg", piezas: "8 Piezas", popular: true },
+    { id: 31, cat: "Especiales", nombre: "Kani Fresh", precio: 14000, ingredientes: "Roll relleno de kanikama, pepino y queso crema. Con cobertura de finas fetas de palta y semillas de sésamo.", imagen: "imagenes/.jpeg", piezas: "8 Piezas" },
+    { id: 32, cat: "Especiales", nombre: "Avocado Pink Fusion", precio: 16000, ingredientes: "Roll relleno de kanikama, palta y queso crema. Con cobertura de finas fetas de salmón rosado fresco.", imagen: "imagenes/.jpeg", piezas: "8 Piezas", popular: true },
+    { id: 33, cat: "Especiales", nombre: "Black Pasion", precio: 16000, ingredientes: "Roll relleno de salmón rosado fresco, queso crema con cebollita de verdeo y palta. Con cobertura de sésamo negro.", imagen: "imagenes/BlackPasion.jpeg", piezas: "8 Piezas" },
+    { id: 34, cat: "Especiales", nombre: "Ebi Red Pasion", precio: 16000, ingredientes: "Roll relleno de langostinos rebozados en panko, queso crema y palta. Cubierto con finas fetas de salmón rosado fresco.", imagen: "imagenes/SalmonCobertura.jpeg", piezas: "8 Piezas", popular: true },
+    { id: 35, cat: "Especiales", nombre: "Palmito Fresh", precio: 13000, ingredientes: "Roll relleno de palmitos, queso crema y palta.", imagen: "imagenes/Palmito.jpeg", piezas: "8 Piezas" },
+    { id: 36, cat: "Especiales", nombre: "California Roll", precio: 13000, ingredientes: "Roll relleno de kanikama, palta y bastoncitos de pepino.", imagen: "imagenes/California.jpg", piezas: "8 Piezas", popular: true },
+    { id: 37, cat: "Especiales", nombre: "Sweet Red (SIN STOCK)", precio: 16000, ingredientes: "Roll relleno de salmón rosado fresco, queso crema y frutilla. Con cobertura de sésamo negro. (Según disponibilidad de fruta de estación). (SIN STOCK)", imagen: "imagenes/SweetRed.jpg", piezas: "8 Piezas", popular: true },
+    //{ id: 38, cat: "Especiales", nombre: "Tropical Roll", precio: 15000, ingredientes: "Roll relleno de salmón rosado fresco, queso crema y mango. Con cobertura de sésamo negro. (Según disponibilidad de fruta de estación)", imagen: "imagenes/Philly.jpg", piezas: "8 Piezas", popular: true },
 
     // ROLLS FRIOS 
-    { id: 4, cat: "Frios", nombre: "Ebi Furai Roll", precio: 13500, ingredientes: "Langostino rebozado, palta y queso. Envuelto en sésamo.", imagen: "imagenes/EviFurai.jfif", piezas: "8 Piezas", popular: true },
-    { id: 5, cat: "Frios", nombre: "Sweet Kani Roll (SIN STOCK)", precio: 13000, ingredientes: "Kanikama, frutilla y queso. Envuelto en sésamo. (SIN STOCK)", imagen: "imagenes/SweetKani.jpeg", piezas: "8 Piezas" },
-    { id: 12, cat: "Frios", nombre: "Red Pasion Roll", precio: 14000, ingredientes: "Salmón, palta y queso. Envuelto por fuera con alga nori.", imagen: "imagenes/RedPasion.jfif", piezas: "8 Piezas", popular: true },
-    { id: 13, cat: "Frios", nombre: "Kani Pink Roll", precio: 12000, ingredientes: "Kanikama, palta y queso. Envuelto por fuera con alga nori.", imagen: "imagenes/Kanikama.jfif", piezas: "8 Piezas" },
-    { id: 14, cat: "Frios", nombre: "Tuna Deluxe Roll", precio: 12000, ingredientes: "Atún saborizado con mayonesa, palta y queso. Envuelto en sésamo.", imagen: "imagenes/TunaDeluxe.jfif", piezas: "8 Piezas" },
-    { id: 15, cat: "Frios", nombre: "Karaage Roll", precio: 12000, ingredientes: "Pollo rebozado, palta y queso. Envuelto en sésamo.", imagen: "imagenes/Karaage.jfif", piezas: "8 Piezas" },
+    { id: 4, cat: "Frios", nombre: "Ebi Furai Roll", precio: 14000, ingredientes: "Langostino rebozado, palta y queso. Envuelto en sésamo.", imagen: "imagenes/EviFurai.jfif", piezas: "8 Piezas", popular: true },
+    { id: 5, cat: "Frios", nombre: "Sweet Kani Roll (SIN STOCK)", precio: 14000, ingredientes: "Kanikama, frutilla y queso. Envuelto en sésamo. (SIN STOCK)", imagen: "imagenes/SweetKani.jpeg", piezas: "8 Piezas" },
+    { id: 12, cat: "Frios", nombre: "Red Pasion Roll", precio: 15000, ingredientes: "Salmón, palta y queso. Envuelto por fuera con alga nori.", imagen: "imagenes/RedPasion.jfif", piezas: "8 Piezas", popular: true },
+    { id: 13, cat: "Frios", nombre: "Kani Pink Roll", precio: 13000, ingredientes: "Kanikama, palta y queso. Envuelto por fuera con alga nori.", imagen: "imagenes/Kanikama.jfif", piezas: "8 Piezas" },
+    { id: 14, cat: "Frios", nombre: "Tuna Deluxe Roll", precio: 13000, ingredientes: "Atún saborizado con mayonesa, palta y queso. Envuelto en sésamo.", imagen: "imagenes/TunaDeluxe.jfif", piezas: "8 Piezas" },
+    { id: 15, cat: "Frios", nombre: "Karaage Roll", precio: 13000, ingredientes: "Pollo rebozado, palta y queso. Envuelto en sésamo.", imagen: "imagenes/Karaage.jfif", piezas: "8 Piezas" },
 
     // ROLLS HOT 
-    { id: 6, cat: "Hot", nombre: "Hot Roll Pasion Deluxe", precio: 15500, ingredientes: "Roll de salmón, palta y queso, rebozado en panko y frito.", imagen: "imagenes/HotRoll.jfif", piezas: "8 Piezas", popular: true },
-    { id: 16, cat: "Hot", nombre: "Hot Roll Ebi Deluxe", precio: 15500, ingredientes: "Langostino, palta y queso rebozado en panko, frito por fuera y caliente por dentro.", imagen: "imagenes/LangostinoHot.jfif", piezas: "8 Piezas", popular: true },
-    { id: 17, cat: "Hot", nombre: "Hot Roll Karaage", precio: 15500, ingredientes: "Pollo crocante, palta y queso, frito en tempura panko.", imagen: "imagenes/PolloHot.jfif", piezas: "8 Piezas" },
-    { id: 20, cat: "Hot", nombre: "Hot Roll Kani Pink", precio: 15500, ingredientes: "Kanikama, palta y queso, rebozado en panko y frito.", imagen: "imagenes/KanikamaHot.jfif", piezas: "8 Piezas" },
+    { id: 6, cat: "Hot", nombre: "Hot Roll Pasion Deluxe", precio: 16000, ingredientes: "Roll de salmón, palta y queso, rebozado en panko y frito.", imagen: "imagenes/HotRoll.jfif", piezas: "8 Piezas", popular: true },
+    { id: 16, cat: "Hot", nombre: "Hot Roll Ebi Deluxe", precio: 16000, ingredientes: "Langostino, palta y queso rebozado en panko, frito por fuera y caliente por dentro.", imagen: "imagenes/LangostinoHot.jfif", piezas: "8 Piezas", popular: true },
+    { id: 17, cat: "Hot", nombre: "Hot Roll Karaage", precio: 16000, ingredientes: "Pollo crocante, palta y queso, frito en tempura panko.", imagen: "imagenes/PolloHot.jfif", piezas: "8 Piezas" },
+    { id: 20, cat: "Hot", nombre: "Hot Roll Kani Pink", precio: 16000, ingredientes: "Kanikama, palta y queso, rebozado en panko y frito.", imagen: "imagenes/KanikamaHot.jfif", piezas: "8 Piezas" },
 
     // VEGGIE
-    { id: 7, cat: "Veggie", nombre: "Veggie Roll", precio: 11000, ingredientes: "Pepino, zanahoria y queso. Envuelto en sésamo.", imagen: "imagenes/SushiVeggie.jpeg", piezas: "8 Piezas" }
+    { id: 7, cat: "Veggie", nombre: "Veggie Roll", precio: 12000, ingredientes: "Pepino, zanahoria y queso. Envuelto en sésamo.", imagen: "imagenes/SushiVeggie.jpeg", piezas: "8 Piezas" }
 ];
 
 // --- LÓGICA GENERAL ---
